3002a030da8f1b7deb71c3222eb5ff64ee586145
Fede <federicojose2000@gmail.com>
2024-06-29 20:45:30 +0200

    fixed misspelled properties

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
           <div className="w-[35px] h-[64px]
           rounded-3xl border-4 border-secondary
           flex justify-center items-start p-2">
-          <motion.dev
+          <motion.div
           animate={{
             y:[0, 24, 0]
           }}
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,22 +1,31 @@
-import { Html } from '@react-three/drei'
-import { progress } from 'framer-motion'
-import React from 'react'
+import { Html, useProgress } from "@react-three/drei";
 
-const Loader = () => {
+const CanvasLoader = () => {
+  const { progress } = useProgress();
   return (
-    <Html>
-      <span className='canvas-load'></span>
-      <p
+    <Html
+      as='div'
+      center
       style={{
-        fontSize: 14,
-        color: '#f1f1f1',
-        fontWeight: 800,
-        marginTop: 40
-      }}>
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <span className='canvas-loader'></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: "#F1F1F1",
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      >
         {progress.toFixed(2)}%
       </p>
     </Html>
-  )
-}
+  );
+};
 
-export default Loader
+export default CanvasLoader;
