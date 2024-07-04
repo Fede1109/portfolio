8bcd3d7ff61a5af7a980c63dde47b8f33b668102
Fede <federicojose2000@gmail.com>
2024-07-04 22:26:14 +0200

    starting work experience section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import Tilt from 'react-tilt';
+import {motion } from 'framer-motion'
+
+import { styles } from '../styles'
+import { github } from '../assets'
+import { SectionWrapper } from '../hoc'
+import { projects } from '../constants/constants'
+import { fadeIn, textVariant } from '../utils/motion';
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+    
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, "")
