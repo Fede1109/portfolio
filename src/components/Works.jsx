2f828b344c14718663a86727742cb1ce8c3ed4ef
Fede <federicojose2000@gmail.com>
2024-07-04 22:34:36 +0200

    startig work section headers

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,5 @@
 import Tilt from 'react-tilt';
-import {motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 
 import { styles } from '../styles'
 import { github } from '../assets'
@@ -10,7 +10,23 @@ import { fadeIn, textVariant } from '../utils/motion';
 const Works = () => {
   return (
     <>
-    
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My work</p>
+        <h2 className={styles.sectionHeadText}>Projects</h2>
+      </motion.div>
+
+      <div className='w-full flex'>
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className='mt-3 text-secondary text-[17px]
+        max-w-3xl leading-[30px]'>
+          Following projects showcases my skills and experience through
+          real-world examples of my work. Each project is briefly described with
+          links to code repositories and live demos in it. It reflects my
+          ability to solve complex problems, work with different technologies,
+          and manage projects effectively.
+        </motion.p>
+      </div>
     </>
   )
 }
