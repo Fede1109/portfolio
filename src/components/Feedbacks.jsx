d985eee78b8e47e69014d098e193cf5182d34869
Fede <federicojose2000@gmail.com>
2024-07-05 19:11:28 +0200

    starting testimonials section

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
-
+import { motion } from "framer-motion"
+import { styles } from "../styles"
+import { SectionWrapper } from "../hoc"
+import { fadeIn, textVariant } from "../utils/motion"
+import { testimonials } from "../constants/constants"
 const Feedbacks = () => {
   return (
     <div>Feedbacks</div>
