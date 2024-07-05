bb7b86cb445b16df742452bcac058944fa485297
Fede <federicojose2000@gmail.com>
2024-07-05 22:49:56 +0200

    main feedback section don

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -9,8 +9,16 @@ const Feedbacks = () => {
       <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
         <motion.div variants={textVariant()}>
           <p className={styles.sectionSubText}>What others say</p>
-          <h2 className={styles.sectionHeadText}>Testimonials</h2>
+          <h2 className={styles.sectionHeadText}>Testimonials.</h2>
         </motion.div>
+      </div>
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard
+          key={testimonial.name}
+          index={index}
+          {...testimonial}/>
+        ))}
       </div>
     </div>
   )
