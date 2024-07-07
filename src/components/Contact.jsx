7614e6afa4367cbe118d6966fcb95775fa735d69
Fede <federicojose2000@gmail.com>
2024-07-07 19:25:45 +0200

    header contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,8 +17,15 @@ const Contact = () => {
   const handleChange = (e) => {}
   const handleSubmit = (e) => {}
   return (
-    <div>Contact</div>
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+      variants={slideIn('left', "tween", 0.2, 1)}
+      className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+      </motion.div>
+    </div>
   )
 }
 
-export default Contact
+export default SectionWrapper(Contact, "contact")
