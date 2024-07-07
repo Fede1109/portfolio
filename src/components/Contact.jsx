fd8f3e050fdcdbcbc34605d1f0a8dd309647eefa
Fede <federicojose2000@gmail.com>
2024-07-07 19:15:59 +0200

    starting contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import { useState, useRef } from "react"
+import { motion } from "framer-motion"
+import emailjs from "@emailjs/browser"
 
+import {styles } from "../styles"
+import { EarthCanvas } from "./canvas"
+import { SectionWrapper} from "../hoc"
+import { slideIn } from "../utils/motion"
 const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    message:''
+  });
   return (
     <div>Contact</div>
   )
