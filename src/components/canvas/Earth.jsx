99d9cd1776e258f59a7d5ac62119cebf99b6324c
Fede <federicojose2000@gmail.com>
2024-07-12 13:20:08 +0200

    Satarting EarthCanvas component

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-
+import { Suspense } from "react"
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
+import CanvasLoader from "../Loader"
 const Earth = () => {
   return (
     <div>Earth</div>
+  )
+}
+
+const EarthCanvas = () => {
+  return(
+    <Canvas>
+      
+    </Canvas>
   )
 }
 
